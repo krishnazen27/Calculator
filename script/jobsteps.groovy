30fed17aca8a9524dd0c52f40b6f08e3689fddad
krishnazen27 <krishnazen27@gmail.com>
2021-03-19 09:25:04 +0530

    testing

---
+++ script/jobsteps.groovy
@@ -0,0 +1,29 @@
+//Declarative pipeline syntax
+pipeline {
+    agent any
+    stages{
+        stage('checkout branch') {
+            steps {
+                script {
+                    sh "ls -la"
+                }
+                println "checkout branch"
+            }
+        }
+        stage('build repo') {
+            steps {
+                println "build code"
+            }
+        }
+        stage('QA test') {
+            steps {
+                println "build code"
+            }
+        }
+        stage('Deploy') {
+            steps {
+                println "build code"
+            }
+        }
+    }
+}
